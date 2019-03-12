1fa6373bbbbe66d8aaf408e93846cbeb362145bc
davidjayjackson <46544155+davidjayjackson@users.noreply.github.com>
2019-03-12 16:54:13 -0400

    Add files via upload
    Initial Python code and Jupyter notebook ; Used Pandas to pull down data from two different souces(csv) and Matplotlab adds some plots.

---
+++ jup.py
@@ -0,0 +1,49 @@
+#%%
+# %matplotlib inline 
+import matplotlib.pyplot as mt
+import pandas as  pd
+
+
+#%%
+spots = pd.read_csv("http://sidc.be/silso/DATA/SN_d_tot_V2.0.csv",sep=";" )
+spots.columns = ["Year","Month","Day", "Fdate","Spots", "Sd","Obs" ,"Defin"]
+spots.head()
+#%%
+## Download NOAA/AAVSO Sunspot and Plot
+noaa = pd.read_csv("https://www.aavso.org/sites/default/files/solar/NOAAfiles/NOAAdaily.csv")
+noaa.head()
+#%%
+x = spots.Fdate
+y = spots.Spots
+mt.plot(x,y)
+mt.title("The Quick Brown Fox")
+mt.xlabel("Year")
+mt.ylabel("Mean Sunspots")
+mt.show()
+#%%
+# Pull out data for 1955 and plot
+s = spots[spots.Year==1955]
+s.head()
+#%%
+x = s.Month
+y = s.Spots
+mt.figure(figsize =(12,8))
+mt.bar(x,y)
+mt.title("Sunspost for my Year of Birth: 1955")
+mt.xlabel("Year")
+mt.ylabel("Mean Sunspots")
+mt.grid(axis='y')
+mt.show()
+#%%
+# Plot (NOAA) sunspot for year >= 2010
+n = noaa[noaa.Year >=2010]
+n.head()
+x = n.Year
+y = n.Ra
+mt.figure(figsize =(12,8))
+mt.bar(x,y)
+mt.title("Mean Sunspots(NOAA) by Year:2010-2019")
+mt.xlabel("Year")
+mt.ylabel("Mean Sunspots")
+mt.grid(axis='y')
+mt.show()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+#%%
+print("The Quick Brown Fox")
